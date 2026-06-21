3e215c3f5bf52f67e57a4a810c8e692ea3ad5539
Ivan Khmelev <el.vasil.va@yandex.ru>
2026-06-21 23:54:20 +0300

    связка бекенда и фронтенда

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,6 +6,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Interview Arena API')
     .setDescription('API для подготовки к собеседованиям')
